3fbf55f6ef24114cc79b660a631887be38b2b772
sajjad <sajjadlogic@live.com>
2019-07-28 14:53:50 +0000

    Remove Unnecessary Files

--- MyProject.WebAPI/Global.asax.cs
+++ MyProject.WebAPI/Global.asax.cs
@@ -16,8 +16,6 @@ namespace MyProject.WebAPI
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
