1b756261d08a573277ab99d4290266087b7b5962
Sajjad Hussain <Sajjadlogic@live.com>
2019-08-16 01:29:19 +0400

    Start working on New Shipment Creation

---
+++ MyProject.WebPresentation/Areas/AdminPanel/Controllers/ShipmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyProject.WebPresentation.Areas.AdminPanel.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        // GET: AdminPanel/Shipment
+        public ActionResult NewShipment()
+        {
+            return View();
+        }
+    }
+}
